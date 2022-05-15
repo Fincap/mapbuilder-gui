db23ffce9d7baca4f1dcc7d2c40399ffc5ad4683
fincap <finnkerman@gmail.com>
2022-05-15 13:43:05 +1000

    Project configuration fixes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 #include <imgui\imgui.h>
+#include <SDL\SDL.h>
+#include <MapBuilderCore.h>
+
 
 int main(int argc, char** argv)
 {
-  std::cout << "Hello template!" << std::endl;
+  return 0;
 }
