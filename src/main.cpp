141a9804989f933986cabbec2fa0bd11eb3d6766
fincap <finnkerman@gmail.com>
2022-05-15 15:22:23 +1000

    Basic IMGUI window with SDL + DX12 context. Closes #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,198 @@
+#define NOMINMAX
+
 #include <iostream>
 
-#include <imgui\imgui.h>
-#include <SDL\SDL.h>
+#include <imgui.h>
+#include <imgui_impl_sdl.h>
+#include <imgui_impl_dx11.h>
+#include <d3d11.h>
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_syswm.h>
 #include <MapBuilderCore.h>
+
+
+// Data
+static ID3D11Device*            g_pd3dDevice = NULL;
+static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
+static IDXGISwapChain*          g_pSwapChain = NULL;
+static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
+
+// Forward declarations of helper functions
+bool CreateDeviceD3D(HWND hwnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
 
 
 int main(int argc, char** argv)
 {
+
+  // Setup SDL
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+    printf("Error: %s", SDL_GetError());
+    exit(1);
+  }
+
+  // Setup window
+  SDL_WindowFlags windowFlags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+  SDL_Window* window = SDL_CreateWindow("MapBuilder", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, windowFlags);
+  SDL_SysWMinfo wmInfo;
+  SDL_VERSION(&wmInfo.version);
+  SDL_GetWindowWMInfo(window, &wmInfo);
+  HWND hwnd = (HWND)wmInfo.info.win.window;
+
+  // Initialize Direct3D
+  if (!CreateDeviceD3D(hwnd))
+  {
+    CleanupDeviceD3D();
+    exit(1);
+  }
+
+  // Setup ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  
+  // Setup ImGui style
+  ImGui::StyleColorsDark();
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplSDL2_InitForD3D(window);
+  ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+  // Our state
+  bool showDemoWindow = true;
+  bool showAnotherWindow = false;
+  ImVec4 clearColour = ImVec4(0.45f, 0.55f, 0.60f, 1.f);
+
+  // Main loop
+  bool done = false;
+  while (!done)
+  {
+    // Poll and handle events
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+      ImGui_ImplSDL2_ProcessEvent(&event);
+
+      // Window quit
+      if (event.type == SDL_QUIT)
+        done = true;
+      if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+        done = true;
+
+      // Window resize
+      if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED && event.window.windowID == SDL_GetWindowID(window))
+      {
+        // Releaes all outstanding references to the swap chain's buffers before resizing
+        CleanupRenderTarget();
+        g_pSwapChain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, 0);
+        CreateRenderTarget();
+      }
+    }
+
+    // Start the ImGui frame
+    ImGui_ImplDX11_NewFrame();
+    ImGui_ImplSDL2_NewFrame(window);
+    ImGui::NewFrame();
+
+    // Show demo window
+    if (showDemoWindow)
+      ImGui::ShowDemoWindow(&showDemoWindow);
+
+    // Show MapBuilder window
+    {
+      ImGui::Begin("MapBuilder");
+      ImGui::Text("Sample text");
+      ImGui::End();
+    }
+
+    // Rendering
+    ImGui::Render();
+    const float clearColourWithAlpha[4] = { 
+      clearColour.x * clearColour.w,
+      clearColour.y * clearColour.w,
+      clearColour.z * clearColour.w,
+      clearColour.w
+    };
+    g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+    g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clearColourWithAlpha);
+    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+    g_pSwapChain->Present(1, 0); // Present with vsync
+
+  }
+
+  // Cleanup
+  ImGui_ImplDX11_Shutdown();
+  ImGui_ImplSDL2_Shutdown();
+  ImGui::DestroyContext();
+
+  CleanupDeviceD3D();
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return 0;
+}
+
+
+// Helper functions
+
+bool CreateDeviceD3D(HWND hWnd)
+{
+  // Setup swap chain
+  DXGI_SWAP_CHAIN_DESC sd;
+  ZeroMemory(&sd, sizeof(sd));
+  sd.BufferCount = 2;
+  sd.BufferDesc.Width = 0;
+  sd.BufferDesc.Height = 0;
+  sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+  sd.BufferDesc.RefreshRate.Numerator = 60;
+  sd.BufferDesc.RefreshRate.Denominator = 1;
+  sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+  sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+  sd.OutputWindow = hWnd;
+  sd.SampleDesc.Count = 1;
+  sd.SampleDesc.Quality = 0;
+  sd.Windowed = TRUE;
+  sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+  UINT createDeviceFlags = 0;
+  //createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+  D3D_FEATURE_LEVEL featureLevel;
+  const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+  if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, 
+    featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel,
+    &g_pd3dDeviceContext) != S_OK
+   )
+    return false;
+
+  CreateRenderTarget();
+  return true;
+}
+
+
+void CleanupDeviceD3D()
+{
+  CleanupRenderTarget();
+  if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
+  if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
+  if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+}
+
+
+void CreateRenderTarget()
+{
+  ID3D11Texture2D* pBackBuffer;
+  g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+  g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+  pBackBuffer->Release();
+}
+
+
+void CleanupRenderTarget()
+{
+  if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
 }
