4b8b8d2209e81f66eb08b5f557ccb4d2c48ac719
fincap <finnkerman@gmail.com>
2022-05-15 16:42:25 +1000

    Added basic menu bar layout.

--- src/app/Application.cpp
+++ src/app/Application.cpp
@@ -2,7 +2,54 @@
 
 void Application::showWindow()
 {
-  ImGui::Begin("MapBuilder");
-  ImGui::Text("Sample text");
-  ImGui::End();
+  _showPlaceholderModal = false;
+
+  showMainMenuBar();
+
+  // Show placeholder if necessary
+  if (_showPlaceholderModal)
+    ImGui::OpenPopup("Placeholder");
+
+  showPlaceholderModal();
+
+}
+
+
+void Application::showPlaceholderModal()
+{
+  // Always center this window when appearing
+  ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+  ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+
+  if (ImGui::BeginPopupModal("Placeholder", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+  {
+    ImGui::Text("Functionality to be implemented.\n\n");
+    ImGui::Separator();
+
+    if (ImGui::Button("OK", ImVec2(120, 0))) { ImGui::CloseCurrentPopup(); }
+    ImGui::SetItemDefaultFocus();
+    ImGui::EndPopup();
+  }
+}
+
+
+void Application::showMainMenuBar()
+{
+  if (ImGui::BeginMainMenuBar())
+  {
+    if (ImGui::BeginMenu("File"))
+    {
+      if (ImGui::MenuItem("New", "CTRL+N")) { _showPlaceholderModal = true; }
+      if (ImGui::MenuItem("Open", "CTRL+O")) { _showPlaceholderModal = true; }
+      if (ImGui::MenuItem("Save", "CTRL+S")) { _showPlaceholderModal = true; }
+      if (ImGui::MenuItem("Save As", "F12")) { _showPlaceholderModal = true; }
+      ImGui::EndMenu();
+    }
+    if (ImGui::BeginMenu("Help"))
+    {
+      if (ImGui::MenuItem("About MapBuilder")) { _showPlaceholderModal = true; }
+      ImGui::EndMenu();
+    }
+    ImGui::EndMainMenuBar();
+  }
 }
--- src/app/Application.h
+++ src/app/Application.h
@@ -8,5 +8,13 @@ class Application
 {
 public:
   void showWindow();
+
+private:
+  // Placeholder popup
+  bool _showPlaceholderModal = false;
+  void showPlaceholderModal();
+
+  // Main menu bar
+  void showMainMenuBar();
 };
 
