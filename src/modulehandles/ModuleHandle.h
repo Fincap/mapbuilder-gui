516a1aea7ae3d88e45ce736bc79f46afd3d2423d
fincap <finnkerman@gmail.com>
2022-05-16 20:08:00 +1000

    Created ModuleHandle abstract base class definition.

--- src/modulehandles/ModuleHandle.h
+++ src/modulehandles/ModuleHandle.h
@@ -1,12 +1,20 @@
 #pragma once
 
+#include <imgui.h>
+
+#include "wrappers\ModuleWrapper.h"
+
 /*
 The base class for UI components displayed in the PipelineView, containing
-a ModuleWrapper and basic display definition. Derived classes are responsible
-for constructing and maintaining the references in the ModuleWrapper member.
+a ModuleWrapper and module display definition. Derived classes are responsible
+for constructing and maintaining the references in the ModuleWrapper member,
+as well as constructing a valid UI.
 */
 class ModuleHandle
 {
+public:
+  virtual void showHandle() = 0;  // Must be implemented by derived.
 
+private:
+  ModuleWrapper _wrapper;
 };
-
