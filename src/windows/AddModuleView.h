488d9b89b726a101262140e061296eea42d65226
fincap <finnkerman@gmail.com>
2022-05-17 21:22:03 +1000

    Refinment of Add Module View definition.

--- src/windows/AddModuleView.h
+++ src/windows/AddModuleView.h
@@ -4,7 +4,8 @@
 #include <imgui.h>
 #include <MapBuilderCore.h>
 
-#include "wrappers/ModuleWrapper.h"
+#include "modules\ModuleInfo.h"
+#include "wrappers\ModuleWrapper.h"
 
 /*
 This window provides a view of all modules that can be added to the Pipeline
@@ -14,12 +15,25 @@ the ability to load Modules dynamically created through add-ons.
 */
 class AddModuleView
 {
+  // Convenience typing for a function that adds a new ModuleWrapper to the
+  // Application's context.
+  using fn_AddModuleWrapperToContext = std::function<void(ModuleWrapper)>;
+
 public:
-  AddModuleView();
+  AddModuleView(fn_AddModuleWrapperToContext);
+
+  void showWindow();          // Display ImGui window.
 
 private:
-  void loadCoreModules();
-  void loadAddonModules();
+  void loadCoreModules();     // Load Modules defined by mapbuilder-core.
+  void loadAddonModules();    // Load Modules defined by user add-on.
+
+  std::vector<ModuleInfo> _modules;   // Loaded-in Modules.
+
+  // Function to be called when a new Module is to be added to the Application
+  // context.
+  // This function should be provided by the calling Application.
+  fn_AddModuleWrapperToContext _addModuleWrapperToContext;
 
 };
 
