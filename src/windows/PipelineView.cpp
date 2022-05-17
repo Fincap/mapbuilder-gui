dbc0e1cc49edb98fa43e6a5e8d4a5a6fe7886a64
fincap <finnkerman@gmail.com>
2022-05-17 18:16:27 +1000

    Basic implementation for Pipeline View window.

--- src/windows/PipelineView.cpp
+++ src/windows/PipelineView.cpp
@@ -1,1 +1,13 @@
 #include "PipelineView.h"
+
+void PipelineView::showWindow(std::vector<ModuleWrapper> modules)
+{
+  ImGui::Begin("Pipeline");
+
+  for (auto& mod : modules)
+  {
+    mod.handle->showHandle();
+  }
+
+  ImGui::End();
+}
