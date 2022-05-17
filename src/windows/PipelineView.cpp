279dabad0f2ec5672cc02c862c173ab87637734b
fincap <finnkerman@gmail.com>
2022-05-17 20:41:23 +1000

    TODO comment:  drag-and-drop for PipelineView.

--- src/windows/PipelineView.cpp
+++ src/windows/PipelineView.cpp
@@ -2,6 +2,9 @@
 
 void PipelineView::showWindow(std::vector<ModuleWrapper>& modules)
 {
+  // TODO Add drag-and-drop reordering of Modules (ideally within their
+  // respective Pipeline Stages).
+  // Refer to lines 2319 through 2343 in imgui_demo.cpp
   ImGui::Begin("Pipeline");
 
   for (auto mod = modules.begin(); mod != modules.end();)
