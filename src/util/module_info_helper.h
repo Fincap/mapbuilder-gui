730cb751943e4c8c4638e5dae8a70540723ca237
fincap <finnkerman@gmail.com>
2022-05-21 15:55:00 +1000

    Created template func for registering ModuleInfo.

---
+++ src/util/module_info_helper.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <MapBuilderCore.h>
+
+#include "modules\ModuleInfo.h"
+
+
+/*
+Templated ModuleInfo struct used for manual registration of ModuleInfo.
+Type M is a derived class of mbc::Module.
+Type H is a derived class of ModuleHandle.
+*/
+template <typename M, typename H>
+struct ManualInfo : public ModuleInfo
+{
+  ManualInfo(const char* _name, const char* _description, mbc::PipelineStage _stage)
+  {
+    name = _name;
+    description = _description;
+    stage = _stage;
+  }
+
+  ModuleWrapper::Ptr create()
+  {
+    auto wrapper = std::make_shared<ModuleWrapper>();
+    wrapper->module = std::make_shared<M>();;
+    wrapper->handle = std::make_shared<H>(wrapper);
+    std::clog << name << " module address: " << &wrapper->module << std::endl;
+    std::clog << name << " handle address: " << &wrapper->handle << std::endl;
+    std::clog << "Finished adding " << name << std::endl;
+    return wrapper;
+  }
+};
