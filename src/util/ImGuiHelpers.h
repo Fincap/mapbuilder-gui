5570d54d36b7e77c30c871c317ca798530378cdb
fincap <finnkerman@gmail.com>
2022-06-05 15:37:27 +1000

    Added helpers to cast between float4 and uint32.

--- src/util/ImGuiHelpers.h
+++ src/util/ImGuiHelpers.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 
 #include <imgui.h>
 
@@ -19,5 +20,26 @@ namespace util
       ImGui::PopTextWrapPos();
       ImGui::EndTooltip();
     }
+  }
+
+  /* Convert uint32_t 0xRRGGBB to ImVec4 for ImGui ColorEdit. */
+  inline ImVec4 ConvertRGBToFloat4(uint32_t in)
+  {
+    float s = 1.0f / 255.0f;
+    return ImVec4(
+      ((in >> 16)  & 0xFF) * s,
+      ((in >> 8)   & 0xFF) * s,
+      ((in)        & 0xFF) * s,
+                    (0xFF) * s);
+  }
+
+  /* Convert ImVec4 to uint32_t 0xRRGGBB for ImGui ColorEdit. */
+  inline uint32_t ConvertFloat4ToRGB(ImVec4 in)
+  {
+    unsigned char r = in.x * 255.f;
+    unsigned char g = in.y * 255.f;
+    unsigned char b = in.z * 255.f;
+
+    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
   }
 }
